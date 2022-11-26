6fdb3832e0d82ac874032226008db71b853e8320
alan88wy <alan88w@yahoo.com>
2022-11-26 23:06:38 +0800

    Added participants etc

--- base/models.py
+++ base/models.py
@@ -17,7 +17,7 @@ class Room(models.Model):
     
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
-    # participants = 
+    participants = models.ManyToManyField(User, related_name="participants", blank=True)
     updated = models.DateTimeField(auto_now=True) # auto_now add timestamp every time we update
     created = models.DateTimeField(auto_now_add=True) # auto_now_add only add first time we created it
     
--- base/views.py
+++ base/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render, redirect
 from django.db.models import Q
 from django.http import HttpResponse
-from .models import Room, Topic
+from .models import Room, Topic, Message
 from .forms import RoomForm
 from django.http import Http404
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 # Queries to DB, template to render etc
@@ -20,11 +21,13 @@ from django.contrib.auth.decorators import login_required
 
 def loginPage(request):
     
+    page = 'login'
+    
     if request.user.is_authenticated:
         return redirect('home')
     
     if request.method == 'POST':
-        username = request.POST.get('username')
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
         
         try:
@@ -40,7 +43,7 @@ def loginPage(request):
         else:
             messages.error(request, 'Login Failed. Username or Password is incorrect !')
             
-    context = {}
+    context = {'page': page}
     
     return render(request, 'base/login_register.html', context)
 
@@ -49,6 +52,28 @@ def logoutUser(request):
     logout(request)
     
     return redirect('home')
+
+def registerPage(request):
+
+    form = UserCreationForm()
+    
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        
+        if form.is_valid():
+            user = form.save(commit=False) # Do not save yet. Let's clean it first
+            user.username = user.username.lower()
+            
+            user.save()
+
+            login(request, user)
+            
+            return redirect('home')
+        else:
+            messages.error = (request, 'An error occurred during registration!')
+    
+    context = {'form': form}
+    return render(request, 'base/login_register.html', context)
 
 def home(request):
     
@@ -76,9 +101,20 @@ def room(request, pk):
     #         room = i
     
     room = Room.objects.get(id=pk)
+    room_messages = room.message_set.all()
+    participants = room.participants.all()
     
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user = request.user,
+            room = room,
+            body = request.POST.get('body'),
+        )
+        
+        room.participants.add(request.user)
+        return redirect('room', pk=room.id)
             
-    context = {'room': room }
+    context = {'room': room, 'room_messages' : room_messages, 'participants': participants }
     return render(request, 'base/room.html', context)
 
 @login_required(login_url = 'login')
@@ -136,3 +172,18 @@ def deleteRoom(request, pk):
     
     return render(request, 'base/delete.html', {'obj': room})
 
+@login_required(login_url = 'login')
+def deleteMessage(request, pk):
+    message = Message.objects.get(id = pk)
+    
+    if request.user != message.user:
+        return HttpResponse("You are not allow to delete this item")
+    
+    # Take care of the input from user
+    if request.method == 'POST':
+        message.delete()
+        return redirect('home')  # Redirect to home page
+    
+    # context = {'form': form}
+    
+    return render(request, 'base/delete.html', {'obj': message})
