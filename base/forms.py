76184e9da9e4e3205a11a6d18618be2765700073
alan88wy <10415010+alan88wy@users.noreply.github.com>
2023-05-21 22:13:53 +0800

    Added comments in forms.py

--- base/forms.py
+++ base/forms.py
@@ -2,6 +2,15 @@ from django.forms import ModelForm
 from .models import Room
 
 class RoomForm(ModelForm):
+    
+    '''
+    Can also do this:
+    
+    name = forms.CharField(label='Name', max_length=200)
+    ....
+    
+    '''
     class Meta:
         model = Room
         fields = '__all__'  # includes all fields from Room
+        
