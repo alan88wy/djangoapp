7f19ff549554a9c210f4565c8174789e0032775a
alan88wy <10415010+alan88wy@users.noreply.github.com>
2023-05-22 23:00:53 +0800

    Added custom css and  statuc_root

--- djangoApp/settings.py
+++ djangoApp/settings.py
@@ -121,10 +121,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
-STATICFILES_DIR = (
+STATIC_URL = 'static/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
+# STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+
+
+STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
-    )
+    # BASE_DIR / "static"
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
